138749ad9817b1838353bc7c546e43190f570908
Michel Icart <michel.c.icart@gmail.com>
2020-08-24 10:57:58 -0300

    review erradas

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,7 +5,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "b1eb6dc79c9bcfb75e43da3653f2ee6a",
 "/": "b1eb6dc79c9bcfb75e43da3653f2ee6a",
-"main.dart.js": "720940da5b478f3b2ddf0c7c6873d023",
+"main.dart.js": "b4188ee9e9e59d6f155f9e01d620a0bb",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
